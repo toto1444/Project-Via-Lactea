82ace110edc213697770d5b7965d7a20ea38a99f
Moon19ht <ryan9947@naver.com>
2020-12-06 04:50:10 +0900

    Interim Inspection
    change whats ojakgyo page

--- Project/ojakgyo/src/views/whatsojakgyo.vue
+++ Project/ojakgyo/src/views/whatsojakgyo.vue
@@ -8,56 +8,65 @@
 -->
 
 <template>
-    <div>
-        <h2>What's Ojakgyo?</h2>
-        <br>
-        <!-- 팀 오작교 소개 -->
-        <p>
-        Team 'Ojakgyo'(오작교)는 인디 게임 개발자와 게임 테스터간의 다리를 놓아주는 "Project Via Lactea" 플랫폼을 제작했습니다.
-        Ojakgyo(오작교)는 설화 '견우와 직녀'에서 까마귀와 까치가 만든 다리라는 뜻으로, 애인 관계인 사람 혹은 친구와 친구 애인 사이를 더 돈독하게 만드는 사람(중개자)를 비유할 때 씁니다.
-        </P>
-        <p>
-        즉, 팀 오작교는 인디 게임 개발자가 따로 베타테스트를 하기 힘들고 홍보하기 힘들 때,
-        이 플랫폼을 사용하면 배타테스터와 홍보를 동시에 잡을 수 있는 기회를 제공하는 다리를 놓아주는 역할이 됩니다.
-        </p>
-        <br>
+  <div class="row">
+    <div class="container">
+      <div class="sidebar four-twelve">
+        <div class="container">
+          <!-- 사이드 about -->
+          <ul class="side-about">
+            <div class="text-right"></div>
+          </ul>
 
-        <!-- 프로젝트 소개 -->
-        <h3>What is "Project Via Lactea"?</h3>
-        <p>"Project Via Lactea"는 인디 게임 개발자와 게임 테스터를 이어 주는 플랫폼입니다.</P>
-        <P>Project Via Lactea는 견우와 직녀 설화의 견우성과 직녀성이 있는 은하수를 뜻하는 영문인 "Milky Way"에서 착안해 이를 라틴어 "Via Lactea"로 번역한 것입니다.</p>
-        <br>
+          <!-- 사이드바 네비게이션 -->
+          <ul class="sidebar-nav">
+            <!-- 사이드바 헤딩 -->
+            <div class="sidebar-heading">
+              <h4>Noticeboard</h4>
+            </div>
+            <li class="sidebar-nav-item"><a id="fake" href="#"></a></li>
+            <li class="sidebar-nav-item"><a id="fake" href="#"></a></li>
+            <li class="sidebar-nav-item"><a id="fake" href="#"></a></li>
+          </ul>
+        </div>
+      </div>
 
-        <!-- 인디게임 정의 -->
-        <h3>What is "Indie Game"?</h3>
-        <p>인디 게임(Indie Game, Independent Game)이란</p>
-        <P>
-        소형 개발사에서 대형 기획사나 게임 회사에서의 지원을 받지 않고, 스스로의 힘으로 제작하거나, 크라우드 펀딩 등의 방법으로만 자금을 조달하여 제작된 게임을 말합니다.
-        인디 게임 개발자는 대부분 잘 알려지지 않은 게임들이 대부분이며, 베타테스터를 모집하기 힘들어서 자잘한 버그가 간간히 발생하는 경우가 빈번해 인디 게임 개발자들이 힘들어 하는 경우가 많습니다.
-        그래서! "Project Via Lactea" 플랫폼이 이를 해결해줄 것입니다.
-        </p>
+      <!-- 메인 페이지 -->
+      <main class="blogcontainer eight-tweleve" role="main">
+        <div class="blog-post">
+          <!-- 팀 오작교 소개 -->
+          <h2 class="entry-title" itemprop="headline">
+            <a id="fake" href="#">팀 오작교(Ojakgyo) 소개</a>
+          </h2>
+          <br />
+          <span class="entry-summary">
+            Team 'Ojakgyo'(오작교)는 인디 게임 개발자와 게임 테스터간의 다리를 놓아주는
+            "Project Via Lactea" 플랫폼을 제작했습니다. Ojakgyo(오작교)는 설화 '견우와
+            직녀'에서 까마귀와 까치가 만든 다리라는 뜻으로, 애인 관계인 사람 혹은 친구와
+            친구 애인 사이를 더 돈독하게 만드는 사람(중개자)를 비유할 때 씁니다. 즉, 팀
+            오작교는 인디 게임 개발자가 따로 베타테스트를 하기 힘들고 홍보하기 힘들 때, 이
+            플랫폼을 사용하면 배타테스터와 홍보를 동시에 잡을 수 있는 기회를 제공하는
+            다리를 놓아주는 역할이 됩니다.
+          </span>
+        </div>
+        <br />
+
+        <!--  -->
+        <div class="blog-post">
+          <h2 class="entry-title" itemprop="headline">
+            <a id="fake" href="#"></a>
+          </h2>
+        </div>
+      </main>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name : "whatsojakgyo"
-}
+  name: "whatsojakgyo",
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Sunflower:Light');
-h2, h3{
-    align-content: center;
-    margin: 40px 0 0;
-    font-family: Sunflower;
-}
-p {
-  background : #73c8b7;
-  text-align : center;
-  font-weight : bold;
-  font-size: 1.5em;
-  padding: 2px;
-  font-family: Sunflower;
-}
+@import url("https://fonts.googleapis.com/css?family=Sunflower:Light");
 </style>
