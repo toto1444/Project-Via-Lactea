81383dcc45611831b62bd5a1bffb822a5adcd959
hanjiwoong <51845043+hanjiung@users.noreply.github.com>
2020-12-05 23:27:08 +0900

    add home loading function of home !

--- Project/ojakgyo/src/components/startScreen.vue
+++ Project/ojakgyo/src/components/startScreen.vue
@@ -1,19 +1,51 @@
 <template>
   <div>
-    <p>qweqwe</p>
+    <img src="../assets/cat.jpg" id="startScreen" />
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'startScreen'
+  name: 'startScreenSource'
 }
 
 </script>
 
 <style scoped>
 
-q
+#startScreen {
+            position: fixed;
+            top:0px;
+            left:0px;
+            background-color: None;
+            animation-duration: 4s;
+            animation-iteration-count: 1;
+            animation-fill-mode :both;
+            z-index: 9999;
+            animation-name: StartScreen;
+        }   
 
+
+@keyframes StartScreen {
+0% {
+    opacity: 1;
+    width: 100%;
+    height: 100%;
+    }
+40%{
+    opacity: 1;
+    }
+90% {
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    }
+
+100%{
+    opacity: 0;
+    width:0%;
+    height:0%;
+    }
+}
 </style>
---
+++ Project/ojakgyo/src/views/team.vue
@@ -0,0 +1,58 @@
+<!--
+***    
+***     스토리 및 내용 구성 담당
+***     이 페이지는 팀원을 소개하는 페이지입니다.
+***     @author     강성룡  kangsungryong
+***     @edit       2020-12-05
+***
+-->
+
+<template>
+    <div>
+        <h2>Team "Ojakgyo"</h2>
+        <br>
+        <ul>
+            <li>한지웅(HanJiUng/HanTimJang)</li>
+            <li>역할 : 메인 개발 및 팀장</li>
+            <li>코멘터리 : </li>
+        </ul>
+        <ul>
+            <li>안아현(AnAhHyeon/toto1444)</li>
+            <li>역할 : 디자이너 및 버그 리폿, 전체 프로젝트 구성 개발자</li>
+            <li>코멘터리 : </li>
+        </ul>
+        <ul>
+            <li>강성룡(KangSungRyong/Ryan)</li>
+            <li>역할 : 시나리오리스트 및 개발자</li>
+            <li>코멘터리 : 에너지 드링크는 제 친구이자 동반자 입니다</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "team"
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Sunflower:Light');
+h2{
+    align-content: center;
+    margin: 40px 0 0;
+    font-family: Sunflower;
+}
+ul {
+  margin: 0px;
+  padding: 0px;
+  list-style-type: none;
+  font-family: Sunflower;
+}
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background: #73c8b7;
+  border: 5px solid #3ab2c4;
+  font-family: Sunflower;
+}
+</style>
