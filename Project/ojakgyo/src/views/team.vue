420e466f77027c216d9fa8b877e19adbb9ce7281
Moon19ht <ryan9947@naver.com>
2020-12-05 18:52:22 +0900

    Write About team vue
    팀 구성원 소개

--- Project/ojakgyo/src/views/team.vue
+++ Project/ojakgyo/src/views/team.vue
@@ -9,17 +9,37 @@
 
 <template>
     <div class="">
-        
+        <h2>Team "Ojakgyo"</h2>
+        <br>
+        <ui>
+            <ll>한지웅(HanJiUng/HanTimJang)</ll>
+            <li>역할 : 메인 개발 및 팀장</li>
+            <li>코멘터리 : </li>
+        </ui>
+        <ul>
+            <li>안아현(AnAhHyeon/toto1444)</li>
+            <li>역할 : 디자이너 및 버그 리폿, 전체 프로젝트 구성 개발자</li>
+            <li>코멘터리 : 젭발 Commit 해주세요, 제발 Please....</li>
+        </ul>
+        <ul>
+            <li>강성룡(KangSungRyong/Ryan)</li>
+            <li>역할 : 시나리오리스트 및 개발자</li>
+            <li>코멘터리 : 에너지 드링크는 제 친구이자 동반자 입니다</li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-    
+    name : "team"
 }
 </script>
 
-    
 <style scoped>
+ul{
 
+}
+ul li{
+
+}
 </style>
