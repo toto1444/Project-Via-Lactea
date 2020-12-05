4dc98f394a88984a3ec360af2138f1e579808e44
hanjiung <hanjiung@hanjiungui-MacBookPro.local>
2020-12-05 18:36:16 +0900

    Corrected the zombieCells.vue documentation. Because the comments are wrong

---
+++ Project/ojakgyo/src/components/vue-like-dislike-buttons.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div>
+    <vue-like-dislike-buttons :likes="876547" :dislikes="4567" likeChecked />
+  </div>
+
+<template>
+
+<script>
+import VueLikeDislikeButtons from 'vue-like-dislike-buttons'
+
+export default {
+  name: 'VueLikeDislikeButtonsSource'
+  components: {
+                VueLikeDislikeButtons
+              }
+}
+</script>
