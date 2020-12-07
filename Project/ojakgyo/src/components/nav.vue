c2e5dc87cd5c3c6c7bda5d3ceb3a1943bd477200
hanjiwoong <51845043+hanjiung@users.noreply.github.com>
2020-12-07 18:37:45 +0900

    competion of nav

--- Project/ojakgyo/src/components/nav.vue
+++ Project/ojakgyo/src/components/nav.vue
@@ -11,14 +11,14 @@ bug 수정: 안아현
 
 <template>
 	<div>
-		<div>
+		<div class="navtotalstyle">
 			<div class="navbgstyle">
-				<router-link to='/'>Home</router-link>
+				<router-link class="homestyle" to='/'>Home</router-link>
 				<button class="downbtn" v-on:click="downupbtn"> Dropdwon </button>
 			</div>
-			<div id="btnshowing">
-				<router-link class="dropa" to="whatsojacgyo">오작교란?</router-link>
-				<router-link class="dropa" to="team">팀소개</router-link>
+			<div id="btnshowing" class="dropstyle">
+				<router-link class="dropa1" to="whatsojacgyo">오작교란?</router-link>
+				<router-link class="dropa2" to="team">팀소개</router-link>
 			</div>
 		</div>
 	</div>
@@ -41,9 +41,22 @@ export default {
 </script>
 
 <style scoped>
+.navtotalstyle {
+	position: fixed;
+	width: 100%;
+	z-index: 9999;
+}
+.dropa1 {
+	border-right: 1px solid #f6f8fa;
+}
+.homestyle {
+	color: #ffffff;
+	padding-right: 10px;
+	padding-left: 10px;
+}
 .navbgstyle {
 	background-color: #3498DB;
-	height: 35px;
+	height: 38px;
 }
 #btnshowing {
 	display: None;
@@ -54,7 +67,14 @@ export default {
 	color: white;
 	border: none;
 }
-
+.dropstyle a {
+	color: white;
+	padding: 10px;
+	display: inline-block;
+	position: relative;
+  left: 58px;
+	background-color: #51a6fc;
+}
 .downbtn:hover .downbtn:focus {
 	background-color: #2980B9;
 }
